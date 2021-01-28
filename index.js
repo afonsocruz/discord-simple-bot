3c18a5fd77c9d62f0e2cbbf6134347e800d4c6ec
Afonso <afonsocruz.rj@gmail.com>
2021-01-28 20:43:05 -0300

    updated bot commands to english texts

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const config = require('./config.json');
 const client = new Discord.Client;
 
 // adding a prefix to bot a command
-
 const prefix = "!";
 
 client.on("message", (message) => {
@@ -17,25 +16,13 @@ client.on("message", (message) => {
 
     if (command == "ping") {
         const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`Pong! Esta mensagem teve uma latência de ${timeTaken}ms.`);
+        message.reply(`Pong! This message took ${timeTaken}ms.`);
     } else if (command === "sum") {
         const numArgs = args.map(x => parseFloat(x));
         const sum = numArgs.reduce((counter, x) => counter += x);
-        message.reply(`A soma é: ${sum}!`);
-    } else if (command === 'comandos') {
-        message.reply("Os comandos são: !ping e !sum")
-    } else if (command === "zaradu") {
-        message.reply("Esse é otário!")
-    } else if (command === "pao") {
-        message.reply("Fracassou na vida toda, burrão!");
-    } else if (command === "high") {
-        message.reply("famoso traficante de petrópolis");
-    } else if (command === "malu") {
-        message.reply("a esperança desse lugar de fracassados");
-    } else if (command === "azeitona") {
-        message.reply("isso é fofoca, BDRRRRRRRR");
-    } else if (command === "isa") {
-        message.reply("pança")
+        message.reply(`the sum is: ${sum}!`);
+    } else if (command === 'commands') {
+        message.reply("The commands are são: !ping and !sum")
     }
 });
 
